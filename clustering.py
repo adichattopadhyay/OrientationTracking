7658919783d732e3123403765fe445d6c9bd7ec1
adichattopadhyay <adichattopadhyay@gmail.com>
2021-03-09 20:01:54 -0500

    Started commenting the code

--- clustering.py
+++ clustering.py
@@ -8,7 +8,6 @@ from scipy import signal
 from scipy import pi
 import numpy as np    
 from scipy.signal import butter, lfilter, freqz
-from kneed import KneeLocator
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
@@ -17,6 +16,14 @@ from sklearn.preprocessing import StandardScaler
 import os
 
 def divideChunk(chunkSize, overlap, data):
+    """
+    chunkSize: The size of each chunk that the data is going to divided into
+    overlap: Fraction. How much overlap is in each chunk, if say you want 90% overlap, you want to input 1/10 due to how overlap is implemented. 
+    data: The data that will be divided
+    
+    This function takes data, divides that data into chunks of size chunkSize with an overlap of overlap 
+    Returns: A list that contains the start and end index of every chunk.
+    """
     nextSample = round(chunkSize*overlap)
     indexList = []
     for i in range(0, len(data), nextSample):
@@ -24,10 +31,20 @@ def divideChunk(chunkSize, overlap, data):
             indexList.append([i, len(data)-1])
         else:
             indexList.append([i,i+chunkSize-1])
-    print("\nFirst 10 for index list:", indexList[0:10])
     return indexList
 
 def nightAvg(windowSize, overlap, data, movement):
+    """
+    windowSize: The size of each window that the data is going to divided into
+    overlap: Fraction. How much overlap is in each window, if say you want 90% overlap, you want to input 1/10 due to how overlap is implemented. 
+    data: The data that will be divided and used to calculate the average pitch and roll for each chunk. This data is meant to be orientation data.
+    movement: A list of the movements
+    
+    This function takes the orientation data and divides it into chunks using the divideChunk() function. 
+    Each chunk is associated with an average pitch and roll which is just the averages of the pitches and rolls in the chunk.
+    Finally, each chunk is classified as a movement chunk if >= 50% of the chunk is movement. If not it is a non-movement chunk.
+    Returns: The average Pitch, average Roll, and movement for every chunk.
+    """
     count = 0
     movCount = 0
     noMovCount = 0
@@ -59,9 +76,14 @@ def nightAvg(windowSize, overlap, data, movement):
     return [avgPitch, avgRoll, movementList]
     
 def orient(acc):
-    #accx = acc[0]
-    #accy = acc[1]
-    #accz = acc[2]
+    """
+    acc: Accelerometer data
+    
+    This function is used to calculate the pitch and roll for every data point in the accelerometer data.
+    Returns: The pitch, roll, and orientation for every data point.
+    """
+    
+    #Phase unwrapping
     accx = list(np.unwrap(np.array(acc[0])*2)/2)
     accy = list(np.unwrap(np.array(acc[1])*2)/2)
     accz = list(np.unwrap(np.array(acc[2])*2)/2)
@@ -77,29 +99,34 @@ def orient(acc):
     return [pitch, roll, orientation]
 
 def windowAvg(windowSize, overlap, data):
+    """
+    windowSize: The size of each window that the data is going to divided into
+    overlap: Fraction. How much overlap is in each window, if say you want 90% overlap, you want to input 1/10 due to how overlap is implemented. 
+    data: The data that will be divided and used to calculate the moving average with
+    
+    This function takes any list of data points and calculates the moving average with a window size windowSize and overlap overLap.
+    Returns: The result of the moving average filter.
+    """
     count = 0
     indexList = divideChunk(windowSize, overlap, data)
     avgData = []
-    print("data len", len(data))
-    print("index len", len(indexList))
     for i in range(len(data)):
-        try:
-            if(indexList[count][0]==i):
-                avg = sum(data[indexList[count][0]:indexList[count][1]])/windowSize  
-                avgData.append(avg)
-                if(count!=len(indexList)-1):
-                    count+=1
-            else:
-                avgData.append(avgData[i-1])
-        except:
-            print("count", count)
-            print("i", i)
-            print("len avgData", len(avgData))
-            print("Last 10 for index list: ", indexList[-10:])
-            exit()
+        if(indexList[count][0]==i):
+            avg = sum(data[indexList[count][0]:indexList[count][1]])/windowSize  
+            avgData.append(avg)
+            if(count!=len(indexList)-1):
+                count+=1
+        else:
+            avgData.append(avgData[i-1])
     return avgData
 
 def expandList(arr):
+    """
+    arr: A double array, e.x. [[1,2][3,4]]
+    
+    This function takes a double array and makes it into a singlular array
+    Returns: A singular array. 
+    """
     arrExpand = []
     for i in range(len(arr)):
         for j in arr[i]:
@@ -107,6 +134,14 @@ def expandList(arr):
     return arrExpand
 
 def noMovOrientation(acc, interMovement):
+    """
+    acc: Accelerometer data
+    interMovement: Double array containing the start and end indexes of the intermovement intervals
+    
+    This function goes through the intermovement periods and calculates the roll and pitch for each data point. 
+    For each specific array that contains a start and end index, it calculates and overall pitch and overall orientation. 
+    Returns: A list of the individual yaw, pitch, and roll, individal orientation, the orientation averages, and a list of the times of each of these points.
+    """
     accx = acc[0]
     accy = acc[1]
     accz = acc[2]
@@ -129,19 +164,23 @@ def noMovOrientation(acc, interMovement):
             pitchVal = math.atan2(accx[j], math.sqrt(accy[j]**2+accz[j]**2)) * 180/math.pi
             roll[i].append(rollVal)
             pitch[i].append(pitchVal)
-            #yaw[i].append()
-            #Roll = atan2(Y, Z) * 180/PI;
-            #Pitch = atan2(X, sqrt(Y*Y + Z*Z)) * 180/PI;
             #From https://samselectronicsprojects.blogspot.com/2014/07/getting-roll-pitch-and-yaw-from-mpu-6050.html 
     for i in range(len(roll)):
         for j in range(len(roll[i])):
-            #orientation[i].append((roll[i][j]+pitch[i][j]+yaw[i][j])/3) For when yaw is implemented
             orientation[i].append((roll[i][j]+pitch[i][j])/2)
     for i in range(len(orientation)):
         orientationAvg.append(sum(orientation[i])/len(orientation[i]))
     return [[yaw, pitch, roll], orientation, orientationAvg, time2]
 
 def finalMovement(movement, threshold, moveValue):
+    """
+    movement: Movement data
+    threshold: The maximum gap length for the gap to be considered movement
+    movValue: The value used to designate movement in the list movement
+    
+    This function goes through the movementList and fills up all the gaps that are <= threshold.
+    Returns: A list with gaps filled in the movement
+    """
     started = False
     count = 0
     finalMovement = []
@@ -156,9 +195,6 @@ def finalMovement(movement, threshold, moveValue):
                 for j in range(count+1):
                     finalMovement.append(np.nan)
                 count = 0
-                #print("I'm in the third elif")
-                #print(len(finalMovement))
-                #print(i)
                 started=False
             else:
                 count+=1
@@ -167,17 +203,17 @@ def finalMovement(movement, threshold, moveValue):
                 finalMovement.append(moveValue)
             count = 0
             started = False
-            #print("I'm in the fourth elif---------------------")
-            #print(len(finalMovement))
-            #print(i)
-    #print("All done")
-    #print(len(finalMovement))
-    #print(len(movement))
     return finalMovement
 
 def combineMovement(accMov, gyroMov, moveValue):
-    #print(len(accMov))
-    #print(len(gyroMov))
+    """
+    accMov: The movement calculated with the accelerometer
+    gyroMov: The movement calculated with the gyroscrope
+    movValue: The value used to designate movement in the lists accMov and gyroMov
+    
+    This function goes through both the accelerometer movement and gyroscope movement and combines the two.
+    Returns: A list with the combined accelerometer and gyroscope movement.  
+    """
     movList = []
     for i in range(len(accMov)):
         if(accMov[i] == moveValue or gyroMov[i] == moveValue):
@@ -193,8 +229,9 @@ def movement(movementData, moveValue):
     """
     movementData: List of when there is movement, binary list, either has np.nan or moveValue for a value
     moveValue: int, whatever value is set to denote motion
+    
     This function goes through movementData and tracks how long the movement is and what index it starts at
-    Returns a list [noMoveLen, indexList]
+    Returns: a list [noMoveLen, indexList]
     """
     moveLen = []
     indexList = []
@@ -222,8 +259,9 @@ def noMovement(movementData, moveValue):
     """
     movementData: List of when there is movement, binary list, either has np.nan or moveValue for a value
     moveValue: int, whatever value is set to denote motion
+    
     This function goes through movementData and tracks how long the gap between the movements is and what index it starts at
-    Returns a list [noMoveLen, indexList]
+    Returns: A list [noMoveLen, indexList]
     """
     noMoveLen = []
     indexList = []
@@ -261,6 +299,12 @@ def butter_highpass_filter(data, cutoff, fs, order=5):
     return y
 
 def rms3(lists):
+    """
+    lists: List of 3 lists all of the same length
+    
+    This function goes through the lists and for each point calculates the Root Mean Squared
+    Returns: A list of the values root mean squared. 
+    """
     rms = []
     for i in range(len(lists[0])):
         rms.append(math.sqrt((1/3)*((lists[0][i]**2)+(lists[1][i]**2)+(lists[2][i]**2))))
@@ -271,6 +315,8 @@ fileName = input("Which file do you want?: ")
 df = pd.read_csv(os.getcwd()+"/Data/"+fileName+".csv")
 
 time = df['time'].to_list()
+
+#Normalization factors
 accx = [x / 4096 for x in df['accelerometerX'].to_list()]
 accy = [x / 4096 for x in df['accelerometerY'].to_list()]
 accz = [x / 4096 for x in df['accelerometerZ'].to_list()]
@@ -278,25 +324,21 @@ gyrox = [x / 131 for x in df['gyroscopeX'].to_list()]
 gyroy = [x / 131 for x in df['gyroscopeY'].to_list()]
 gyroz = [x / 131 for x in df['gyroscopeZ'].to_list()]
 
+#Centers the data at 0
 accx2 = butter_highpass_filter(accx, 0.2, 25)
 accy2 = butter_highpass_filter(accy, 0.2, 25)
 accz2 = butter_highpass_filter(accz, 0.2, 25) 
-
-accRMS = rms3([accx2, accy2, accz2])
-
 gyrox2 = butter_highpass_filter(gyrox, 0.05, 25)
 gyroy2 = butter_highpass_filter(gyroy, 0.05, 25)
 gyroz2 = butter_highpass_filter(gyroz, 0.05, 25) 
 
+#Calculates the RMS of the data
+accRMS = rms3([accx2, accy2, accz2])
 gyroRMS = rms3([gyrox2, gyroy2, gyroz2])
 
+#Thresholds of the data
 accLowerThreshold = 0.1
-accUpperThreshold = 8000
 gyroLowerThreshold = 10
-gyroUpperThreshold = 8000
-
-#accMovement = [10000 if accRMS[i] >= accLowerThreshold and accRMS[i] <= accUpperThreshold else np.nan for i in range(len(accRMS))]
-#gyroMovement = [10000 if gyroRMS[i] >= gyroLowerThreshold and gyroRMS[i] <= gyroUpperThreshold else np.nan for i in range(len(gyroRMS))]
 
 #Movement 
 accMovement = [1 if accRMS[i] >= accLowerThreshold else np.nan for i in range(len(accRMS))]
@@ -374,30 +416,20 @@ print("\n------------------------Time------------------------\n")
 print("Time:")
 print(interMovTime[0:20])
 print(interMovTime[72:92])
+         
 
-#Graph the roll and pitch intermovement interval
-#If they look consistent then there is no preprocessing step needed
-#Across the five nights
-#Forget about yaw for now
-
-#print(len(pitch))
-#print(pitch[1540:1570])            
-
-#Expands the roll, pitch, and yaw lists
-#yawExpand = expandList(yaw)
-
-
+#Expands the roll and pitch lists and takes the moving average of them
 pitchExpand = expandList(pitch)
 rollExpand = expandList(roll)
 pitchAvg = windowAvg(100, 9/10, pitchExpand)
 rollAvg = windowAvg(100, 9/10, rollExpand)
+
 pitchFinal = []
 rollFinal = []
 cntr = 0
-
-
 wait=False
 
+#Inserts a NaN at every place that isn't movement 
 for i in range(len(time)):
     if(i>len(interMovTime)-1):
         pitchFinal.append(np.nan)
@@ -410,10 +442,11 @@ for i in range(len(time)):
         rollFinal.append(np.nan)
         cntr+=1
 
+#Second orientation calculation, but for the whole data
 orientation2 = orient([accx2, accy2, accz2])
-#s = int(input("How many seconds do you want the window to be: ")) 
+
+#Divides the orientation into chunks
 s = 60
-print(len(movListFinal))
 nightAverage = nightAvg(s*45, 1, orientation2, movListFinal)
 
 print("\n------------------------Night Average------------------------\n")
@@ -438,6 +471,7 @@ df1 = pd.DataFrame(
 
 nC = int(input("How many clusters?: "))
 
+#K-means clustering
 kmeans = KMeans(n_clusters=nC).fit(df1)
 centroids = kmeans.cluster_centers_
 print(centroids)
@@ -450,7 +484,6 @@ graph = int(input("What graph, scatter (1), or overlay(2)?: "))
 if(graph==1):
     colors = ['red', 'green']
     fig = plt.figure(figsize=(4.5,4.5))
-    #plt.scatter(nightAverage[0], nightAverage[1], c=nightAverage[2], cmap=matplotlib.colors.ListedColormap(colors))
     plt.scatter(nightAverage[0], nightAverage[1],  c= kmeans.labels_.astype(float), s=50, alpha=0.5)
     plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
     plt.title('Pitch vs Roll (' + str(s) + " seconds)")
@@ -462,63 +495,18 @@ if(graph==1):
 elif(graph==2):    
     line1, = plt.plot(time, accRMS, label='accRMS')
     line2, = plt.plot(time, movListFinal, label='Movement Final')
+    alt = True
     for i in range(0, len(time), s*45):
         a = int(i/(s*45))
-        plt.text(time[i], 0, kmeans.labels_[a], fontsize=10, color='red')
-    plt.ylabel('Accelerometer RMS (*insert units*)')
-    plt.title('Accelerator RMS overlayed with movement and cluster')
+        if(alt):
+            plt.text(time[i], 0, kmeans.labels_[a], fontsize=10, color='red')
+            alt = not alt
+        else:
+            plt.text(time[i], 0.1, kmeans.labels_[a], fontsize=10, color='red')
+            alt = not alt
+    plt.ylabel('Accelerometer RMS (g)')
+    plt.title('Accelerometer RMS overlayed with movement and cluster')
     plt.xlabel('Time (ms)')
     plt.legend(loc='best')
     plt.grid(True)
-
-
     plt.show()
-
-"""
-Do phase unwrapping before clustering https://scikit-image.org/docs/dev/auto_examples/filters/plot_phase_unwrap.html
-Overlay overnight data with the cluster
-    Name the clusters 1, 2, 3, 4, ...
-    Each cluster appears at a certain time, at that time, the value of the graph I'm plotting coresponds to a cluster
-    So per window, overlay it with the id
-    Show the accRMS
-    Look at the example PNG
-
-"""
-
-"""
-plt.subplot(2,1,1)
-line1, = plt.plot(time, pitchFinal, label='pitch')
-line2, = plt.plot(time, movListFinal, label='Movement Final')
-plt.ylabel('Pitch (*insert units*)')
-plt.title('Pitch')
-plt.xlabel('Time (ms)')
-plt.legend(loc='best')
-plt.grid(True)
-
-plt.subplot(2,1,2)
-line3, = plt.plot(time, rollFinal, label='roll')
-line4, = plt.plot(time, movListFinal, label='Movement Final')
-plt.ylabel('Roll (*insert units*)')
-plt.title('Roll')
-plt.xlabel('Time (ms)')
-plt.legend(loc='best')
-plt.grid(True)
-
-plt.show()
-"""
-"""
-Take the whole night of data
-Divide it into 30 second windows (30*25=750 samples)
-Keep an overlap factor like with the other one. For the timebeing have 0 overlap
-For each window:
-    1. Average Pitch
-    2. Average Roll
-    3. Moving vs Not
-        If > 50% moving, then person is moving
-        If < 50% moving, then person is not moving
-Plot a scatter plot of the pitch (x) vs the roll (y)
-Color of dots:
-    1. Moving
-    2. Not moving
-    
-"""
